3bc3019e011201299fafcbb9fdf5f78afa651fdb
Pedro95076 <73479140+Pedro95076@users.noreply.github.com>
2021-03-19 16:37:43 +0000

    Adicionei a funçao main e o ficheiro parser.h

---
+++ main.c
@@ -0,0 +1,26 @@
+/**
+ *  @file Ficheiro que contém a função principal do programa
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "parser.h"
+
+/**
+ * \brief Esta é a função principal do programa
+ * 
+ * Esta função recebe uma sequência de caracteres e forma uma string "line"
+ * 
+ * @returns O valor 0
+ */
+int main () {
+
+	char line[10240];
+
+	assert( fgets(line, 10240, stdin) != NULL);
+	assert( line [strlen(line) - 1] == '\n');   // Se conseguir encontrar o \n no final quer dizer que leu a linha toda
+
+    parse(line);
+	return 0;
+}
---
+++ parser.h
@@ -0,0 +1,4 @@
+void PUSH(long i);
+long POP();
+void PRINT_STACK(int x);
+void parse (char *line);
