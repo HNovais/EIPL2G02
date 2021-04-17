c1ddbe2e4b24f7455d3a729f0c03076bca1f4977
dianamateus10 <dianarmateus1998@gmail.com>
2021-04-17 14:48:02 +0100

    acrescentei alterações no ficheiro parser.c

--- parser.c
+++ parser.c
@@ -9,14 +9,12 @@
 #include <math.h>
 
 /**
- * \brief Esta variável é a lista que guarda os elementos da stack, em tipo dados.
+ * \brief Esta é a função que cria a Stack
+ *
+ * @returns Stack
  */
-DADOS stack[10240];
-
-/**
- * \brief Esta variável serve de contador do número de elementos da stack.
- */
-int count = 0;
+STACK *novaStack(){
+    return malloc(sizeof (STACK));
 
 /**
  * \brief Esta é a função que vai fazer o parse de uma linha
@@ -25,406 +23,341 @@ int count = 0;
  *
  * @param line : A linha que foi lida e da qual se vai fazer o parse.
  */
-void parse (char *line) {
+    void parse (char *line) {
 
-    int x, y;
-    long Yl, Xl;
-    double Yd, Xd;
-    char Xc, *Xs, *delims = " \t\n";
+        STACK *stack = novaStack();
+        stack->count = 0;
 
-    for (char *token = strtok(line, delims); token != NULL; token = strtok(NULL, delims))
-    {
-        char *sobra;
-        long val_i = strtol(token, &sobra, 10);
-        if (strlen(sobra) == 0)
+        char *delims = " \t\n";
+        DADOS variaveis [26];
+        // Variáveis com valor por omissão
+        // A = 10
+        variaveis['A'-65].tipo = 0;
+        variaveis['A'-65].vl = 10;
+        // B = 11
+        variaveis['B'-65].tipo = 0;
+        variaveis['B'-65].vl = 11;
+        // C = 12
+        variaveis['C'-65].tipo = 0;
+        variaveis['C'-65].vl = 12;
+        // D = 13
+        variaveis['D'-65].tipo = 0;
+        variaveis['D'-65].vl = 13;
+        // E = 14
+        variaveis['E'-65].tipo = 0;
+        variaveis['E'-65].vl = 14;
+        // F = 15
+        variaveis['F'-65].tipo = 0;
+        variaveis['F'-65].vl = 15;
+        // N = '\n'
+        variaveis['N'-65].tipo = 2;
+        variaveis['N'-65].vc = '\n';
+        // S = ' '
+        variaveis['S'-65].tipo = 2;
+        variaveis['S'-65].vc = ' ';
+        // X = 0
+        variaveis['X'-65].tipo = 0;
+        variaveis['X'-65].vl = 0;
+        // Y = 1
+        variaveis['Y'-65].tipo = 0;
+        variaveis['Y'-65].vl = 1;
+        // Z = 2
+        variaveis['Z'-65].tipo = 0;
+        variaveis['Z'-65].vl = 2;
+
+        for (char *token = strtok(line, delims); token != NULL; token = strtok(NULL, delims))
         {
-            PUSHL(val_i);
-        }
-        else
-        {
-            double val_d = strtod(token, &sobra);
+            char *sobra;
+            long val_i = strtol(token, &sobra, 10);
             if (strlen(sobra) == 0)
             {
-                PUSHD(val_d);
+                PUSHL(stack, val_i);
             }
-            else if (strcmp(token, "+") == 0)
+            else
             {
-                x = stack[count - 1].tipo;
-                if (x == 0) {Yl = POPL();Yd = 0.0;}
-                else {Yd = POPD();Yl = 0;}
-                y = stack[count - 1].tipo;
-                if (y == 0) {Xl = POPL();Xd = 0.0;}
-                else {Xd = POPD();Xl = 0;}
-                if (x == 0 && y == 0) PUSHL(Xl + Yl);
-                else PUSHD(Xl + Xd + Yl + Yd);
-            }
-            else if (strcmp(token, "-") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) {Yl = POPL();Yd = 0.0;}
-                else {Yd = POPD();Yl = 0;}
-                y = stack[count - 1].tipo;
-                if (y == 0) {Xl = POPL();Xd = 0.0;}
-                else {Xd = POPD();Xl = 0;}
-                if (x == 0 && y == 0) PUSHL(Xl - Yl);
-                else PUSHD((Xl + Xd) - (Yl + Yd));
-            }
-            else if (strcmp(token, "*") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) {Yl = POPL();Yd = 0.0;}
-                else {Yd = POPD();Yl = 0;}
-                y = stack[count - 1].tipo;
-                if (y == 0) {Xl = POPL();Xd = 0.0;}
-                else {Xd = POPD();Xl = 0;}
-                if (x == 0 && y == 0) PUSHL(Xl * Yl);
-                else PUSHD((Xl + Xd) * (Yl + Yd));
-            }
-            else if (strcmp(token, "/") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) {Yl = POPL();Yd = 0.0;}
-                else {Yd = POPD();Yl = 0;}
-                y = stack[count - 1].tipo;
-                if (y == 0) {Xl = POPL();Xd = 0.0;}
-                else {Xd = POPD();Xl = 0;}
-                if (x == 0 && y == 0) PUSHL(Xl / Yl);
-                else PUSHD((Xl + Xd) / (Yl + Yd));
-            }
-            else if (strcmp(token, "(") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) {Xl = POPL();PUSHL(Xl - 1);}
-                else if (x==1) {Xd = POPD();PUSHD(Xd - 1);}
-                else if (x==2) {Xc = POPC();PUSHC(Xc -1);}
-            }
-            else if (strcmp(token, ")") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) {Xl = POPL();PUSHL(Xl + 1);}
-                else if (x==1) {Xd = POPD();PUSHD(Xd + 1);}
-                else if (x==2) {Xc = POPC();PUSHC(Xc +1);}
-            }
-            else if (strcmp(token, "%") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) { Yl = POPL();}
-                y = stack[count - 1].tipo;
-                if (y == 0) { Xl = POPL();}
-                if (x == 0 && y == 0) PUSHL(Xl % Yl);
-            }
-            else if (strcmp(token, "#") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) {Yl = POPL();Yd = 0.0;}
-                else {Yd = POPD();Yl = 0;}
-                y = stack[count - 1].tipo;
-                if (y == 0) {Xl = POPL();Xd = 0.0;}
-                else {Xd = POPD();Xl = 0;}
-                if (x == 0 && y == 0) PUSHL(pow(Xl, Yl));
-                else PUSHD(pow((Xl + Xd), (Yl + Yd)));
-            }else if (strcmp(token, "&") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) { Yl = POPL();}
-                y = stack[count - 1].tipo;
-                if (y == 0) { Xl = POPL();}
-                if (x == 0 && y == 0) PUSHL(Xl & Yl);
-            }
-            else if (strcmp(token, "|") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) { Yl = POPL();}
-                y = stack[count - 1].tipo;
-                if (y == 0) { Xl = POPL();}
-                if (x == 0 && y == 0) PUSHL(Xl | Yl);
-            }
-            else if (strcmp(token, "^") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) { Yl = POPL();}
-                y = stack[count - 1].tipo;
-                if (y == 0) { Xl = POPL();}
-                if (x == 0 && y == 0) PUSHL(Xl ^ Yl);
-            }
-            else if (strcmp(token, "~") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) {Yl = POPL();PUSHL(~Yl);}
-            }
-            else if (strcmp(token, "_") == 0)
-            {
-                DADOS P = POP();
-                PUSH(P);
-                PUSH(P);
-            }
-            else if (strcmp(token, ";") == 0)
-            {
-                POP();
-            }
-            else if (strcmp(token, "\\") == 0)
-            {
-                DADOS P = POP();
-                DADOS Z = POP();
-                PUSH(P);
-                PUSH(Z);
-            }
-            else if (strcmp(token, "@") == 0)
-            {
-                DADOS P = POP();
-                DADOS Z = POP();
-                DADOS T = POP();
-                PUSH(Z);
-                PUSH(P);
-                PUSH(T);
-            }
-            else if (strcmp(token, "$") == 0)
-            {
-                long n = POPL();
-                PUSH(stack[count - n - 1]);
-            }
-            else if (strcmp(token, "i") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 1) {Yd = POPD();PUSHL(Yd);}
-                else if (x == 2) {Xc = POPC(); PUSHL(Xc);}
-                else if (x==3) {Xs = POPS(); PUSHL(atol(Xs));}
-            }
-            else if (strcmp(token, "f") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) {Yl = POPL(); PUSHD(Yl);}
-                else if (x==3) {Xs = POPS(); PUSHD(atof(Xs));}
-            }
-            else if (strcmp(token, "c") == 0)
-            {
-                x = stack[count - 1].tipo;
-                if (x == 0) {Yl = POPL(); PUSHC(Yl);}
-                else if (x==3) {Xs = POPS(); PUSHC(atol(Xs));}
-            }
-            else if (strcmp(token, "s") == 0)
-            {
-                //x = stack[count - 1].tipo;
-                //if (x == 0) {Yl = POPL();PUSHS(ltoa(Yl, Xs, 10));}
-                //else if (x==1) {Yd = POPD(); PUSHS(snprintf (Xs, count,stack[count], Yd));}
-                //else if (x==2) {Xc = POPC(); PUSHS(Xc);}
-            }
-            else if (strcmp(token, "l") == 0)
-            {
-                char s[10240];
-                assert (fgets(s, 10240, stdin) != NULL);
-                PUSHS(s);
+                double val_d = strtod(token, &sobra);
+                if (strlen(sobra) == 0)
+                {
+                    PUSHD(stack, val_d);
+                }
+                else
+                {
+                    (strcmp(token,"+")==0)  ? soma(stack) :
+                    (strcmp(token,"-")==0)  ? subtracao(stack) :
+                    (strcmp(token, "*")==0) ? multiplicacao(stack) :
+                    (strcmp(token, "/")==0) ? divisao(stack) :
+                    (strcmp(token, "(")==0) ? incrementar(stack) :
+                    (strcmp(token, ")")==0) ? decrementar(stack) :
+                    (strcmp(token, "%")==0) ? restoDivInt(stack) :
+                    (strcmp(token, "#")==0) ? exponenciacao(stack) :
+                    (strcmp(token, "&")==0) ? and(stack) :
+                    (strcmp(token, "|")==0) ? or(stack) :
+                    (strcmp(token, "^")==0) ? xor(stack) :
+                    (strcmp(token, "~")==0) ? not(stack) :
+                    (strcmp(token, "_")==0) ? duplicar(stack) :
+                    (strcmp(token, ";")==0) ? lastOut(stack) :
+                    (strcmp(token, "\\")==0)? switchTwo(stack) :
+                    (strcmp(token, "@")==0) ? rotateThree(stack) :
+                    (strcmp(token, "$")==0) ? copyN(stack) :
+                    (strcmp(token, "i")==0) ? convInteiro(stack) :
+                    (strcmp(token, "f")==0) ? convDouble(stack) :
+                    (strcmp(token, "c")==0) ? convCaracter(stack) :
+                    (strcmp(token, "s")==0) ? convString(stack) :
+                    (strcmp(token, "l")==0) ? lerLinha(stack) :
+                    (strcmp(token, "=")==0) ? igual(stack) :
+                    (strcmp(token, "<")==0) ? menor(stack) :
+                    (strcmp(token, ">")==0) ? maior(stack) :
+                    (strcmp(token, "!")==0) ? nao(stack) :
+                    (strcmp(token, "e&")==0) ? conjuncao(stack) :
+                    (strcmp(token, "e|")==0) ? disjuncao(stack) :
+                    (strcmp(token, "e<")==0) ? menorDe2(stack) :
+                    (strcmp(token, "e>")==0) ? maiorDe2(stack) :
+                    (strcmp(token, "?")==0)  ? ifThenElse(stack) :
+                    (variableTeste(token)==0)  ? variableOut(stack, token, variaveis) :
+                    (twoPointsTeste(token)==0) ? variableIn(stack, token, variaveis) :
+                    doNothing();
+                }
             }
         }
+        PRINT_STACK(stack);
     }
-    PRINT_STACK(count);
-}
+
+/**
+ * \brief Esta é a função auxiliar da função parse que permite o término do expression base
+ */
+    void doNothing()
+    {}
 
 /**
  * \brief Esta é a função auxiliar da PRINT_STACK que imprime um elemento dos diferentes tipos de dados
  *
  * @param d : É o elemento a ser imprimido
  */
-void print_DADOS (DADOS d) {
-    switch (d.tipo)
-    {
-        case 0 : printf("%ld", d.vl); break;
-        case 1 : printf("%g", d.vd); break;
-        case 2 : printf("%c", d.vc); break;
-        case 3 : printf("%s", d.vs); break;
+    void print_DADOS (DADOS d) {
+        switch (d.tipo)
+        {
+            case 0 : printf("%ld", d.vl); break;
+            case 1 : printf("%g", d.vd); break;
+            case 2 : printf("%c", d.vc); break;
+            case 3 : printf("%s", d.vs); break;
+        }
     }
-}
 
 /**
  * \brief Esta é a função auxiliar da parse que imprime a stack
  *
- * @param x : É o parametro que indica o tamanho da stack que será imprimida
+ * @param stk: stack
  */
-void PRINT_STACK(int x) {
-    int i;
-    for (i = 0; i<x; i++) {
-        print_DADOS (stack[i]);
+    void PRINT_STACK(STACK *stk) {
+        int i;
+        for (i = 0; i<stk->count; i++) {
+            print_DADOS (stk->comp[i]);
+        }
+        putchar('\n');
     }
-    putchar('\n');
-}
 
 /**
  * \brief Esta é a função auxiliar que retira o último elemento do tipo long da stack
  *
+ * @param stk: stack
+ *
  * @returns O último elemento da stack
  */
-long POPL() {
-    long x;
-    x = stack[count -1].vl;
-    count--;
-    return x;
-}
+    long POPL(STACK *stk) {
+        long x;
+        x = stk->comp[stk->count -1].vl;
+        stk->count--;
+        return x;
+    }
 
 /**
  * \brief Esta é a função auxiliar que retira o último elemento do tipo double da stack
  *
+ * @param stk: stack
+ *
  * @returns O último elemento da stack
  */
-double POPD() {
-    double x;
-    x = stack[count -1].vd;
-    count--;
-    return x;
-}
+    double POPD(STACK *stk) {
+        double x;
+        x = stk->comp[stk->count -1].vd;
+        stk->count--;
+        return x;
+    }
 
 /**
  * \brief Esta é a função auxiliar que retira o último elemento do tipo char da stack
  *
+ * @param stk: stack
+ *
  * @returns O último elemento da stack
  */
-char POPC() {
-    char x;
-    x = stack[count -1].vc;
-    count--;
-    return x;
-}
+    char POPC(STACK *stk) {
+        char x;
+        x = stk->comp[stk->count -1].vc;
+        stk->count--;
+        return x;
+    }
 
 /**
  * \brief Esta é a função auxiliar que retira o último elemento do tipo string da stack
  *
+ * @param stk: stack
+ *
  * @returns O último elemento da stack
  */
-char *POPS()
-{
-    char *x = stack[count-1].vs;
-    count--;
-    return x;
-}
+    char *POPS(STACK *stk)
+    {
+        char *x = stk->comp[stk->count-1].vs;
+        stk->count--;
+        return x;
+    }
 
 /**
  * \brief Esta é a função auxiliar que retira o último elemento da stack
  *
+ * @param stk: stack
+ *
  * @returns O último elemento da stack
  */
-DADOS POP()
-{
-    DADOS P = stack[count-1];
-    count--;
-    return P;
-}
+    DADOS POP(STACK *stk)
+    {
+        DADOS P = stk->comp[stk->count-1];
+        stk->count--;
+        return P;
+    }
 
 /**
  * \brief Esta é a função auxiliar que coloca um elemento do tipo long na stack
  *
  * @param x : É o elemento que queremos colocar na stack
+ * @param stk: stack
  */
-void PUSHL(long x) {
-    stack[count].tipo = 0;
-    stack[count].vl = x;
-    count++;
-}
+    void PUSHL(STACK *stk, long x) {
+        stk->comp[stk->count].tipo = 0;
+        stk->comp[stk->count].vl = x;
+        stk->count++;
+    }
 
 /**
  * \brief Esta é a função auxiliar que coloca um elemento do tipo double na stack
  *
  * @param x : É o elemento que queremos colocar na stack
+ * @param stk: stack
  */
-void PUSHD(double x) {
-    stack[count].tipo = 1;
-    stack[count].vd = x;
-    count++;
-}
+    void PUSHD(STACK *stk, double x) {
+        stk->comp[stk->count].tipo = 1;
+        stk->comp[stk->count].vd = x;
+        stk->count++;
+    }
 
 /**
  * \brief Esta é a função auxiliar que coloca um elemento do tipo char na stack
  *
  * @param x : É o elemento que queremos colocar na stack
+ * @param stk: stack
  */
-void PUSHC(char x)
-{
-    stack[count].tipo = 2;
-    stack[count].vc = x;
-    count++;
-}
+    void PUSHC(STACK *stk, char x)
+    {
+        stk->comp[stk->count].tipo = 2;
+        stk->comp[stk->count].vc = x;
+        stk->count++;
+    }
 
 /**
  * \brief Esta é a função auxiliar que coloca um elemento do tipo string na stack
  *
  * @param x : É o elemento que queremos colocar na stack
+ * @param stk: stack
  */
-void PUSHS(char *x)
-{
-    stack[count].tipo = 3;
-    stack[count].vs = x;
-    count++;
-}
+    void PUSHS(STACK *stk, char *x)
+    {
+        stk->comp[stk->count].tipo = 3;
+        stk->comp[stk->count].vs = x;
+        stk->count++;
+    }
 
 /**
  * \brief Esta é a função auxiliar que põe um elemento na stack
  *
  * @param P : É o elemento que queremos colocar na stack
+ * @param stk: stack
  */
-void PUSH(DADOS P)
-{
-    stack[count] = P;
-    count++;
-}
+    void PUSH(STACK *stk, DADOS P)
+    {
+        stk->comp[stk->count] = P;
+        stk->count++;
+    }
 
 /**
  * \brief Esta é a função auxiliar que converte um long em dados
  *
  * @param x : É o elemento a ser convertido
+ * @param stk: stack
+ *
+ * @returns Retorna uma estrutura do tipo Dados com o valor pretendido
  */
-DADOS longTOdados (long x) {
-    DADOS P;
-    P.tipo = 0;
-    P.vl = x;
+    DADOS longTOdados (long x) {
+        DADOS P;
+        P.tipo = 0;
+        P.vl = x;
 
-    return P;
-}
+        return P;
+    }
 
 /**
  * \brief Esta é a função auxiliar que converte um double em dados
  *
  * @param x : É o elemento a ser convertido
+ *
+ * @returns Retorna uma estrutura do tipo Dados com o valor pretendido
  */
-DADOS doubleTOdados (double x) {
-    DADOS P;
-    P.tipo = 1;
-    P.vd = x;
+    DADOS doubleTOdados (double x) {
+        DADOS P;
+        P.tipo = 1;
+        P.vd = x;
 
-    return P;
-}
+        return P;
+    }
 
 /**
  * \brief Esta é a função auxiliar que converte um char em dados
  *
  * @param x : É o elemento a ser convertido
+ *
+ * @returns Retorna uma estrutura do tipo Dados com o valor pretendido
  */
-DADOS charTOdados (char x) {
-    DADOS P;
-    P.tipo = 2;
-    P.vc = x;
+    DADOS charTOdados (char x) {
+        DADOS P;
+        P.tipo = 2;
+        P.vc = x;
 
-    return P;
-}
+        return P;
+    }
 
 /**
  * \brief Esta é a função auxiliar que converte um elemento do tipo dados em long
  *
  * @param P : É o elemento a ser convertido
+ *
+ * @returns O valor no tipo long presente na estrutura de Dados
  */
-long dadosTOlong (DADOS P) {
-    long x;
-    x = P.vl;
+    long dadosTOlong (DADOS P) {
+        long x;
+        x = P.vl;
 
-    return x;
-}
+        return x;
+    }
 
 /**
  * \brief Esta é a função auxiliar que converte um elemento do tipo dados em double
  *
  * @param P : É o elemento a ser convertido
+ *
+ * @returns O valor no tipo double presente na estrutura de Dados
  */
-double dadosTOdouble (DADOS P) {
-    double x;
-    x = P.vd;
+    double dadosTOdouble (DADOS P) {
+        double x;
+        x = P.vd;
 
-    return x;
-}
-
+        return x;
+    }
